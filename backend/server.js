1a91e202e764bf953d14e4e03e63cb2dfd2d2119
Vinayak kudlamath <vinayakmkudlamath@gmail@example.com>
2026-03-09 21:26:40 +0530

    Added backed

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/contact",(req,res)=>{
+
+    const {name,email,message} = req.body;
+
+    console.log("New Message:");
+    console.log(name,email,message);
+
+    res.json({
+        success:true
+    });
+
+});
+
+app.listen(5000,()=>{
+    console.log("Server running on http://localhost:5000");
+});
